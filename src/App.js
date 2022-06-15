aca23503d5925da81f301a3b598e1c1632c8cbc2
ramees kasim <rameeschuthomandy@gmail.com>
2022-06-15 11:01:28 +0400

    metamask connection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, React, useEffect, useState } from "react";
+import "./App.css";
+import detectEthereumProvider from "@metamask/detect-provider";
+import { Button, Paper, Typography } from "@mui/material";
 
-function App() {
+const App = () => {
+  const [provider, setProvider] = useState(null);
+  const [chainID, setChainID] = useState(null);
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [currentAccountBalance, setCurrentAccountBalance] = useState(null);
+  const [accountList, setAccountList] = useState([]);
+
+  useEffect(() => {
+    metaMask();
+  }, []);
+
+  provider &&
+    provider.on("accountsChanged", async (accounts) => {
+      if (provider) {
+        getAccountList();
+        setCurrentAccountBalance(null);
+      }
+    });
+
+  const metaMask = async () => {
+    let provider_name = await detectEthereumProvider();
+    if (provider_name) {
+      await setProvider(provider_name);
+    }
+  };
+
+  useEffect(() => {
+    getAccountList();
+  }, [provider]);
+
+  const getAccountList = async () => {
+    const chain_id = await provider.request({
+      method: "eth_chainId",
+    });
+    await setChainID(chain_id);
+    provider
+      .request({ method: "eth_accounts" })
+      .then((data) => {
+        setAccountList(data);
+        if (data.length > 0) setCurrentAccount(data[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const downlodMetaMask = () => {
+    window.open(
+      "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn",
+      "_blank"
+    );
+    setProvider(null);
+  };
+
+  const connectMetaMask = async () => {
+    provider
+      .request({ method: "eth_requestAccounts" })
+      .then((data) => {
+        setAccountList(data);
+        data.length > 0 && setCurrentAccount(data[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getBalance = async () => {
+    provider
+      .request({ method: "eth_getBalance", params: [currentAccount, "latest"] })
+      .then((data) => {
+        setCurrentAccountBalance(parseInt(data, 16));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const changeAccount = () => {
+    provider.request({ method: "eth_requestAccounts" });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Paper
+          elevation={2}
+          sx={{
+            p: 2,
+            aspectRatio: "1/.5",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
         >
-          Learn React
-        </a>
+          {!provider ? (
+            <Button variant="outlined" onClick={downlodMetaMask}>
+              Download metaMask extension
+            </Button>
+          ) : accountList.length === 0 ? (
+            <Button variant="outlined" onClick={connectMetaMask}>
+              Connect MetaMask
+            </Button>
+          ) : (
+            <Fragment>
+              <Typography variant="body2">
+                <b>Account : </b>
+                {currentAccount}
+              </Typography>
+              <Typography variant="body2">
+                <b>Balance : </b>${currentAccountBalance}
+              </Typography>
+              <Button variant="outlined" onClick={getBalance}>
+                Get Balance{" "}
+              </Button>
+            </Fragment>
+          )}
+        </Paper>
       </header>
     </div>
   );
-}
+};
 
 export default App;
